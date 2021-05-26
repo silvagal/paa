7a8b6132d291a488a3463452ff862a5977f71c2a
silvagal <57956770+silvagal@users.noreply.github.com>
2021-05-25 23:07:55 -0300

    Add files via upload

---
+++ lista4/max_dc.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <vector>
+
+void generateInstances(std::vector<int>& instances){
+    for (int i = 0; i < instances.size(); ++i)
+        instances[i] = rand() % 100;
+}
+
+template <class T>
+int maximumValue(std::vector<T>& numbers){
+    int n = numbers.size();
+    if (n == 1)
+        return numbers[0];
+    auto first = numbers.cbegin();
+    auto last = numbers.cbegin() + n / 2;
+    std::vector<T> new_vec1(first, last);
+
+
+    first = numbers.cbegin() + n / 2;
+    last = numbers.cend();
+    std::vector<T> new_vec2(first, last);
+
+    T value1 = maximumValue(new_vec1);
+    T value2 = maximumValue(new_vec2);
+    if( value1 > value2)
+        return value1;
+    else
+        return value2;
+}
+
+int main() {
+    int n = 40;
+    std::vector<int> instances(n, 0);
+    generateInstances(instances);
+
+    std::cout << "Instances: " << std::endl;
+    for (auto i : instances)
+        std::cout << i << " ";
+    std::cout << std::endl;
+
+    int max = maximumValue(instances);
+    std::cout << "Greatest: " << max;
+    return 0;
+}
